f7dee64aad9a56d5b0f8ecc8a1508aace736ff81
Michael Volkhardt <michael.volkhardt@gmail.com>
2026-06-25 20:38:18 +0200

    part1 complete

--- src/c/main.c
+++ src/c/main.c
@@ -19,7 +19,7 @@ static void update_time() {
 
   // Write the current date into a buffer
   static char s_date_buffer[16];
-  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %b %d", tick_time);
+  strftime(s_date_buffer, sizeof(s_date_buffer), "%a %d. %b", tick_time);
 
   // Display the date
   text_layer_set_text(s_date_layer, s_date_buffer);
@@ -36,7 +36,7 @@ static void main_window_load(Window *window) {
 
   // Create the time TextLayer
   s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
+      GRect(0, PBL_IF_ROUND_ELSE(110, 104), bounds.size.w, 50));
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorWhite);
   text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
@@ -44,7 +44,7 @@ static void main_window_load(Window *window) {
 
   // Create the date TextLayer
   s_date_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(110, 104), bounds.size.w, 30));
+      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 30));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
   text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));

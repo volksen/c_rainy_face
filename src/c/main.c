6e05df8167ccc19809c50abc108c4fc403252bc2
Michael Volkhardt <michael.volkhardt@gmail.com>
2026-06-25 21:44:49 +0200

    part 2: custom font

--- src/c/main.c
+++ src/c/main.c
@@ -4,6 +4,10 @@ static Window *s_main_window;
 static TextLayer *s_time_layer;
 static TextLayer *s_date_layer;
 
+static GFont s_time_font;
+static GFont s_date_font;
+
+
 static void update_time() {
   // Get a tm structure
   time_t temp = time(NULL);
@@ -30,25 +34,38 @@ static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
 }
 
 static void main_window_load(Window *window) {
+
   // Get information about the Window
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  // Create the time TextLayer
-  s_time_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(110, 104), bounds.size.w, 50));
-  text_layer_set_background_color(s_time_layer, GColorClear);
-  text_layer_set_text_color(s_time_layer, GColorWhite);
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
-  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+  // Load custom fonts
+  s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_JERSEY_64));
+  s_date_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_JERSEY_24));
+
+  // Center the time + date block vertically
+  int date_height = 24;
+  int time_height = 64;
+  int block_height = time_height + date_height;
+  int date_y = (bounds.size.h / 2) - (block_height / 2) - 10;
+  int time_y = date_y + date_height;
 
   // Create the date TextLayer
-  s_date_layer = text_layer_create(
-      GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 30));
+  s_date_layer = text_layer_create(GRect(0, date_y, bounds.size.w, date_height));
   text_layer_set_background_color(s_date_layer, GColorClear);
   text_layer_set_text_color(s_date_layer, GColorWhite);
-  text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  // text_layer_set_font(s_date_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_font(s_date_layer, s_date_font);
   text_layer_set_text_alignment(s_date_layer, GTextAlignmentCenter);
+
+  // Create the time TextLayer
+  s_time_layer = text_layer_create(GRect(0, time_y, bounds.size.w, time_height+10));
+  text_layer_set_background_color(s_time_layer, GColorClear);
+  text_layer_set_text_color(s_time_layer, GColorWhite);
+  text_layer_set_font(s_time_layer, s_time_font);
+  // text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
+
 
   // Add layers to the Window
   layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
@@ -59,6 +76,9 @@ static void main_window_unload(Window *window) {
   // Destroy TextLayers
   text_layer_destroy(s_time_layer);
   text_layer_destroy(s_date_layer);
+  // Unload custom fonts
+  fonts_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_date_font);
 }
 
 static void init() {
